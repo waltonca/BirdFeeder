eb4d85e806c103aa7a095320a13f62c825675d95
Walton <zjxcdut@gmail.com>
2023-05-04 11:10:15 -0300

    change the weather api to daily api

--- script/bundle.js
+++ script/bundle.js
@@ -43,7 +43,7 @@ client.on('connect', function() {
         console.log(`temperature: ${sensorsJson.temp_c}`);
         console.log(`humidity: ${sensorsJson.humidity}`);
         document.getElementById("temperature_c").innerHTML = sensorsJson.temp_c;
-        document.getElementById("temperature_f").innerHTML = sensorsJson.temp_f;
+        // document.getElementById("temperature_f").innerHTML = sensorsJson.temp_f;
         document.getElementById("humidity").innerHTML = sensorsJson.humidity;
       }
     
@@ -65,9 +65,9 @@ client.on('error', function(error) {
 
 
 // TODO: call weather API to get weather data
-// API: https://api.openweathermap.org/data/2.5/forecast?lat=44.668987&lon=-63.613506&appid=d9c2f7d833ba0d49857b4dfa60ddb3bc&units=metric
+// API: https://api.openweathermap.org/data/2.5/forecast/daily?lat=44.668987&lon=-63.613506&appid=20571ab45c74dc2a1897b60c5b8047a1&units=metric
 // Step1: get weather data
-const API_KEY = 'd9c2f7d833ba0d49857b4dfa60ddb3bc';
+const API_KEY = '20571ab45c74dc2a1897b60c5b8047a1';
 const LOCATION_LAT = '44.668987';
 const LOCATION_LON = '-63.613506';
 
@@ -78,31 +78,31 @@ request.onreadystatechange = function() {
         const response = JSON.parse(this.responseText);
         console.log(response);
         // Step2: display weather data
-        // 5 days, only get 00:00:00 data, create a new array to store the data
-        const weatherData = [];
-        for (let i = 0; i < response.list.length; i++) {
-            if (response.list[i].dt_txt.includes('00:00:00')) {
-                weatherData.push(response.list[i]);
-            }
-        }
-        console.log(weatherData);
+        // 7 days weather data
+        // const weatherData = [];
+        // // for (let i = 0; i < response.list.length; i++) {
+        // //     if (response.list[i].dt_txt.includes('00:00:00')) {
+        // //         weatherData.push(response.list[i]);
+        // //     }
+        // // }
+        // console.log(weatherData);
         // display 5 days weather data
-        for (let i = 0; i < weatherData.length; i++) {
-            const date = new Date(weatherData[i].dt * 1000);
-            const day = date.getDay();
-            const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            const dayOfWeek = dayList[day];
-            const temp = weatherData[i].main.temp;
-            const weather = weatherData[i].weather[0].main;
-            console.log(`day: ${dayOfWeek}, temp: ${temp}, weather: ${weather}`);
-            // render data
-            document.getElementById("weather").innerHTML += `day: ${dayOfWeek}, temp: ${temp}, weather: ${weather}<br>`;    
-        }
+          
+            // <div class="sc-AxhUy fxWvvr">
+            //   <h6>Fri</h6>
+            //   <svg width="48" height="48" viewBox="0 0 48 48"><path fill="#BBDEFB" d="M29.5 5A8.5 8.5 0 1 0 29.5 22A8.5 8.5 0 1 0 29.5 5Z"></path><path fill="#BBDEFB" d="M37 14.893A7 7 0 1 0 37 28.893 7 7 0 1 0 37 14.893zM11 15A7 7 0 1 0 11 29 7 7 0 1 0 11 15z"></path><path fill="#BBDEFB" d="M17.5 8A6.5 6.5 0 1 0 17.5 21A6.5 6.5 0 1 0 17.5 8Z"></path><path fill="#BBDEFB" d="M25 12.893A7 7 0 1 0 25 26.893A7 7 0 1 0 25 12.893Z"></path><path fill="#BBDEFB" d="M7,25c0,2.209,1.791,4,4,4h25c2.209,0,4-1.791,4-4v-1c0-2.209-1.791-4-4-4H11c-2.209,0-4,1.791-4,4V25z"></path><g><path fill="#2196F3" d="M34.95 37.15c-1.132 1.133-2.968 1.133-4.101 0-1.134-1.131-1.133-2.969 0-4.1C31.982 31.917 37 31 37 31S36.082 36.02 34.95 37.15zM23.95 41.15c-1.132 1.132-2.968 1.132-4.101 0-1.133-1.133-1.132-2.969 0-4.101 1.133-1.133 6.152-2.05 6.152-2.05S25.082 40.02 23.95 41.15zM13.95 37.15c-1.132 1.133-2.968 1.133-4.101 0-1.133-1.131-1.132-2.969 0-4.1 1.132-1.133 6.151-2.05 6.151-2.05S15.082 36.02 13.95 37.15z"></path></g></svg>
+            //   <p>Rain</p>
+            //   <span>6<sup>°</sup><small>/</small>2<sup>°</sup></span>
+            // </div>
+
+            // check weather
+            let Icon;
+            
     } else {
         console.log("error");
     }
 };
-request.open(`GET`, `https://api.openweathermap.org/data/2.5/forecast?lat=${LOCATION_LAT}&lon=${LOCATION_LON}&appid=${API_KEY}&units=metric`);
+request.open(`GET`, `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${LOCATION_LAT}&lon=${LOCATION_LON}&appid=${API_KEY}&units=metric`);
 request.send();
 
 
